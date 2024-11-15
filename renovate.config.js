1ab7f30ff369e201cea9f9879a10370e4c087acb
Munir Xavier Wanis <munir.wanis@n26.com>
2024-11-15 15:38:52 +0100

    Adds one more command to postUpgradeCommands

--- renovate.config.js
+++ renovate.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   allowedPostUpgradeCommands: [
-    "install-tool ruby \\d+.\\d+.\\d+ && install-gem cocoapods \\d+.\\d+.\\d+ && pod install",
+    "install-tool ruby \\d+.\\d+.\\d+ && install-gem cocoapods \\d+.\\d+.\\d+ && pod install && find \\w+ -name Podfile -execdir pod install \\;",
   ],
   allowPostUpgradeCommandTemplating: true,
 }
